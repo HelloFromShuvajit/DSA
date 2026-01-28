32c0db21ccc8f766b983e027deff3f05bca5f30d
HelloFromShuvajit <shuvajit09191999.com>
2026-01-28 10:07:56 +0530

    ProductofArrayExceptItself

--- ProductofArrayExceptItself.java
+++ ProductofArrayExceptItself.java
@@ -17,23 +17,25 @@ public class ProductofArrayExceptItself {
         int pointer=0;
         for(int num : result){
             System.out.println("product at index "+pointer+" is: "+num);
-            System.out.println(num);
+            pointer++;
         }
-
     }
     public int[] product(int[] arr){
-        int pointer=0;
-        int[] result = new int[arr.length];
-        for(pointer=0;pointer<arr.length;pointer++){
-            int product=1;
-             
-            for(int i=0; i<arr.length;i++){
-                if (pointer!=i) {
-                    product= product*arr[i];
-                }
+        int length=arr.length;
+        int[] left=new int[length];
+        int[] right = new int[length];
+        int[] result = new int[length];
+        left[0]=1;
+        for(int i=1;i<=length-1;i++){
+            left[i]= left[i-1]*arr[i-1];
             }
-            result[pointer]= product;
-        }
+        right[length-1]=1;
+        for(int i=length-2; i>=0;i--){
+            right[i]= right[i+1]*arr[i+1];
+            }
+        for(int i=0;i<length;i++){
+            result[i]= left[i]*right[i];
+            }
     return result;
     }
     }
